9cd85e547b9b363f2bc7a3c2e22d780e74739cf5
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-03-06 20:47:49 -0600

    react branch initialized

---
+++ src/views/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/styles.css";
+
+const App = () => {
+  return (
+    <Fragment>
+      <p>Hola mundo</p>
+    </Fragment>
+  );
+};
+
+export default App;
